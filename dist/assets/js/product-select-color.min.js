bd831287b3e807ee5355bab7df1e447a9be08b6d
akoshevoi <53705782+akoshevoi@users.noreply.github.com>
2020-05-13 22:38:38 +0300

    remove dist

--- dist/assets/js/product-select-color.min.js
+++
@@ -1,7 +0,0 @@
-var selectList = document.querySelectorAll('.select__list');
-
-for (var i = 0; i < selectList.length; i++) {
-  selectList[i].onchange = function () {
-    this.previousElementSibling.style.display='none';
-  }
-}
